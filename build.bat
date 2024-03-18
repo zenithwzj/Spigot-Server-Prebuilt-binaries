c35cdd58a9089b4aea573e700dbdb819c427b21e
zenithwzj <zenithwzj@qq.com>
2024-03-18 16:45:21 +0800

    Build 1.15.2 with java8

--- build.bat
+++ build.bat
@@ -22,14 +22,14 @@ move /Y BuildTools.jar SPBuild >nul
 cd SPBuild
 
 echo [build.bat] Building Spigot...
-java -jar BuildTools.jar --rev 1.16.5 
-if not exist spigot-1.16.5.jar (
+java -jar BuildTools.jar --rev 1.15.2 
+if not exist spigot-1.15.2.jar (
     echo [build.bat] Error Building Spigot! The script is going to exit...
     exit
 ) else echo [build.bat] Build success!
 cd ..
 
 echo [build.bat] Exporting built artifacts ...
-move /Y SPBuild\spigot-1.16.5.jar .\ >nul
+move /Y SPBuild\spigot-1.15.2.jar .\ >nul
 echo [build.bat] Removing src ...
 rd /S /Q SPBuild
