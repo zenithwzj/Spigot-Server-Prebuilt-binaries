a3d45ca1ab0218405f3d6bf0857e63501ea097fe
zenithwzj <zenithwzj@qq.com>
2024-03-18 16:17:54 +0800

    Build 1.17 with openjdk16

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
 @echo off
-echo [build.bat] Downloading openjdk17.zip ...
-curl -L https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_windows-x64_bin.zip --output openjdk17.zip
-if not exist openjdk17.zip (
-    echo [build.bat] Failed to download openjdk17.zip! The script is going to exit...
+echo [build.bat] Downloading openjdk16.zip ...
+curl -L https://download.java.net/java/GA/jdk16.0.2/d4a915d82b4c4fbb9bde534da945d746/7/GPL/openjdk-16.0.2_windows-x64_bin.zip --output openjdk16.zip
+if not exist openjdk16.zip (
+    echo [build.bat] Failed to download openjdk16.zip! The script is going to exit...
     exit
 )
-echo [build.bat] Unpacking openjdk17.zip ...
-tar -xf openjdk17.zip
-del /F /Q openjdk17.zip
+echo [build.bat] Unpacking openjdk16.zip ...
+tar -xf openjdk16.zip
+del /F /Q openjdk16.zip
 
 echo [build.bat] Downloading BuildTools.jar ...
 curl -L https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar --output BuildTools.jar
@@ -22,7 +22,7 @@ move /Y BuildTools.jar SPBuild >nul
 cd SPBuild
 
 echo [build.bat] Building Spigot...
-..\jdk-17.0.2\bin\java -jar BuildTools.jar --rev 1.17 
+..\jdk-16.0.2\bin\java -jar BuildTools.jar --rev 1.17 
 if not exist spigot-1.17.jar (
     echo [build.bat] Error Building Spigot! The script is going to exit...
     exit
