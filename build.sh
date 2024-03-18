cef17624c9be5277ef8e51483a394edc109116e4
zenithwzj <zenithwzj@qq.com>
2024-03-18 17:42:40 +0800

    REBuild BTv1.11,on linux,with java 8

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+echo '[build] Downloading java8u401 archive ...'
+curl -L https://javadl.oracle.com/webapps/download/AutoDL?BundleId=249542_4d245f941845490c91360409ecffb3b4 --output java8.tar.gz
+if ! [ -e java8.tar.gz ] ;then
+    >&2 echo '[build] Failed to download java8.tar.gz! The script is going to exit...'
+    exit
+fi
+tar -zxf java8.tar.gz
+
+
 echo '[build] Downloading BuildTools.jar ...'
 curl -L https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar --output BuildTools.jar
 if ! [ -e BuildTools.jar ] ;then
@@ -11,7 +20,7 @@ mv -f BuildTools.jar SPBuild >/dev/null
 cd SPBuild
 
 echo [build] Building Spigot...
-java -jar BuildTools.jar --rev 1.11 
+./jre1.8.0_401/bin/java -jar BuildTools.jar --rev 1.11 
 if ! [ -e spigot-*.jar ] ;then
     >&2 echo '[build] Error Building Spigot! The script is going to exit...'
     exit
