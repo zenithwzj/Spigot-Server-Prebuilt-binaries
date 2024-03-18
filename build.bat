1cef47f9029f2d85f72b3ecfee4925ae6889e7d5
zenithwzj <zenithwzj@qq.com>
2024-03-18 16:21:26 +0800

    Build 1.16 with openjdk16

--- build.bat
+++ build.bat
@@ -22,14 +22,14 @@ move /Y BuildTools.jar SPBuild >nul
 cd SPBuild
 
 echo [build.bat] Building Spigot...
-..\jdk-16.0.2\bin\java -jar BuildTools.jar --rev 1.17 
-if not exist spigot-1.17.jar (
+..\jdk-16.0.2\bin\java -jar BuildTools.jar --rev 1.16 
+if not exist spigot-1.16.jar (
     echo [build.bat] Error Building Spigot! The script is going to exit...
     exit
 ) else echo [build.bat] Build success!
 cd ..
 
 echo [build.bat] Exporting built artifacts ...
-move /Y SPBuild\spigot-1.17.jar .\ >nul
+move /Y SPBuild\spigot-1.16.jar .\ >nul
 echo [build.bat] Removing src ...
 rd /S /Q SPBuild
