96312eb196f66b3bd3b9a0aa90f440ac69dd472c
zenithwzj <zenithwzj@qq.com>
2024-03-18 15:42:13 +0800

    Build1.20.2

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
 @echo off
-@REM Partly copied from my private repository
-
 echo [build.bat] Downloading openjdk21.zip ...
 curl -L https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_windows-x64_bin.zip --output openjdk21.zip
 if not exist openjdk21.zip (
@@ -24,16 +22,14 @@ move /Y BuildTools.jar SPBuild >nul
 cd SPBuild
 
 echo [build.bat] Building Spigot...
-..\jdk-21.0.2\bin\java -jar BuildTools.jar --rev 1.20.4 
-if not exist spigot-1.20.4.jar (
+..\jdk-21.0.2\bin\java -jar BuildTools.jar --rev 1.20.2 
+if not exist spigot-1.20.2.jar (
     echo [build.bat] Error Building Spigot! The script is going to exit...
     exit
 ) else echo [build.bat] Build success!
 cd ..
 
 echo [build.bat] Exporting built artifacts ...
-move /Y SPBuild\spigot-1.20.4.jar .\ >nul
+move /Y SPBuild\spigot-1.20.2.jar .\ >nul
 echo [build.bat] Removing src ...
 rd /S /Q SPBuild
-
-@REM artifact:spigot-1.20.4.jar
